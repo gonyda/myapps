5afb342a1b593e4f2d138e67e9b78b480afeba88
gony <bbsk3939@gmail.com>
2026-06-29 13:54:24 +0900

    chore(mycrawler): 불필요한 package-info.java 파일 제거

--- mycrawler/src/main/java/com/myapps/web/mycrawler/application/service/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * 크롤러 애플리케이션 서비스 패키지.
- *
- * <p>유스케이스 오케스트레이션 및 스케줄 관리를 담당합니다.
- */
-package com.myapps.web.mycrawler.application.service;
--- mycrawler/src/main/java/com/myapps/web/mycrawler/interfaces/api/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * 크롤러 인터페이스 API 패키지.
- *
- * <p>HTTP 요청 처리 및 웹 UI 렌더링을 담당하는 컨트롤러를 포함합니다.
- */
-package com.myapps.web.mycrawler.interfaces.api;
